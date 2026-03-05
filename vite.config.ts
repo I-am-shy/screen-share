decaca1390f375edbd5e9700fa6875b4bf61a68a
i-am-shy <2050808169@qq.com>
2026-03-06 01:33:08 +0800

    部署设置强制刷新缓存（补充）

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,16 @@ export default defineConfig({
     // 注入全局变量，让构建内容包含这个戳（触发哈希变化）
     __BUILD_VERSION__: JSON.stringify(Date.now().toString())
   },
+  build: {
+    // 可选：明确开启资产哈希（Vite 默认开启，写出来更清晰）
+    assetsInlineLimit: 4096, // 小于4kb的资源内联，不生成单独文件
+    rollupOptions: {
+      output: {
+        // 强制指定哈希格式（name=文件名，hash=内容哈希）
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      }
+    }
+  }
 })
