b7406268b9d29c74aa8df5c33ffddd37ef42aeb3
i-am-shy <2050808169@qq.com>
2026-03-06 01:26:26 +0800

    部署设置强制刷新缓存

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,9 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true
-  }
+  },
+  define: {
+    // 注入全局变量，让构建内容包含这个戳（触发哈希变化）
+    __BUILD_VERSION__: JSON.stringify(Date.now().toString())
+  },
 })
